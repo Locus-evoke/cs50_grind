ac13bc00783929e5f27d659b690f4b045139cf0d
Locus <164335346+Ascendant-Bias@users.noreply.github.com>
2024-06-25 19:20:10 +0500

    Update library.py

--- library.py
+++ library.py
@@ -1,1 +1,13 @@
 #test
+import emoji
+
+lol = input('Input: ')
+print(list(lol))
+if len(list(lol)) > 1:
+    a,b = lol.split(' ', maxsplit=1)
+    print(a,b)
+    m = emoji.get_emoji_by_name(f'{b}', 'en')
+    print(a,m)
+else:
+    m = emoji.get_emoji_by_name(f'{lol}', 'en')
+    print(m)
